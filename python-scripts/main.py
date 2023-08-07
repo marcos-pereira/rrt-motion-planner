95eb6a64ea1c3ccc1c94257e2567f7c7ec4ebbb0
Marcos Pereira <marcos.si.pereira@gmail.com>
2023-08-07 01:26:09 +0100

    [main.py] Call refactored RRT planner.

--- python-scripts/main.py
+++ python-scripts/main.py
@@ -2,7 +2,9 @@
 from rtree import index
 from RRTPlannerMapPyglet import RRTPlanner
 from RRTStarPlannerMapPyglet import RRTStarPlanner
+from RRT import RRT
 from Map import load_map
+from PlanDrawer import PlanDrawer
 
 def main():
     x_init = (30, 30)
@@ -18,21 +20,34 @@ def main():
     
     scene_map = load_map(map_name)
     
+    rrt_planner = RRT(x_init,
+                    x_goal,
+                    goal_radius,
+                    steer_delta,
+                    map_name,
+                    scene_map,
+                    num_nodes,
+                    font_size)
+    
+    map_height, map_width = scene_map.shape
+    plan_drawer = PlanDrawer(map_name, map_width, map_height, font_size)
+    plan_drawer.run(rrt_planner)
+    
     ## Call RRT
-    rows, cols = scene_map.shape
-    rrt_planner = RRTPlanner(x_init,
-                             x_goal,
-                             goal_radius,
-                             steer_delta,
-                             map_name,
-                             scene_map,
-                             search_window,
-                             cols,
-                             rows,
-                             num_nodes,
-                             font_size)
+    # rows, cols = scene_map.shape
+    # rrt_planner = RRTPlanner(x_init,
+    #                          x_goal,
+    #                          goal_radius,
+    #                          steer_delta,
+    #                          map_name,
+    #                          scene_map,
+    #                          search_window,
+    #                          cols,
+    #                          rows,
+    #                          num_nodes,
+    #                          font_size)
 
-    rrt_planner.run()
+    # rrt_planner.run()
     
     ## Call RRTStar
     rows, cols = scene_map.shape
