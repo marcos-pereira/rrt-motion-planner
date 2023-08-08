87a78ac6ec422f2cc8b745dda25b244dbf265892
Marcos Pereira <marcos.si.pereira@gmail.com>
2023-08-08 22:31:28 +0100

    [main.py] Comment out older imports and call only RRTStar for now while we fix it.

--- python-scripts/main.py
+++ python-scripts/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from rtree import index
-from RRTPlannerMapPyglet import RRTPlanner
-from RRTStarPlannerMapPyglet import RRTStarPlanner
+# from RRTPlannerMapPyglet import RRTPlanner
+# from RRTStarPlannerMapPyglet import RRTStarPlanner
 from RRT import RRT
+from RRTStar import RRTStar
 from Map import load_map
 from PlanDrawer import PlanDrawer
 
@@ -10,7 +11,7 @@ def main():
     x_init = (30, 30)
     x_goal = (30, 460)
     goal_radius = 10
-    steer_delta = 15
+    steer_delta = 20
     near_radius = 30
     search_window = 800 # 400, 800, 1024
     frames_steps = 1
@@ -25,11 +26,11 @@ def main():
                     goal_radius,
                     steer_delta,
                     scene_map,
-                    num_nodes
+                    num_nodes)
     
     map_height, map_width = scene_map.shape
-    plan_drawer = PlanDrawer(map_name, map_width, map_height, font_size)
-    plan_drawer.run(rrt_planner)
+    # plan_drawer = PlanDrawer(map_name, map_width, map_height, font_size)
+    # plan_drawer.run(rrt_planner)
     
     ## Call RRT
     # rows, cols = scene_map.shape
@@ -47,21 +48,33 @@ def main():
 
     # rrt_planner.run()
     
+    gamma_rrt = 1000
+    rrt_planner = RRTStar(x_init,
+                    x_goal,
+                    goal_radius,
+                    steer_delta,
+                    gamma_rrt,
+                    scene_map,
+                    num_nodes)
+    
+    plan_drawer = PlanDrawer(map_name, map_width, map_height, font_size)
+    plan_drawer.run_forever(rrt_planner)
+    
     ## Call RRTStar
-    rows, cols = scene_map.shape
-    rrt_planner = RRTStarPlanner(x_init,
-                                 x_goal,
-                                 goal_radius,
-                                 steer_delta,
-                                 map_name,
-                                 scene_map,
-                                 search_window,
-                                 cols,
-                                 rows,
-                                 num_nodes,
-                                 font_size)
+    # rows, cols = scene_map.shape
+    # rrt_planner = RRTStarPlanner(x_init,
+    #                              x_goal,
+    #                              goal_radius,
+    #                              steer_delta,
+    #                              map_name,
+    #                              scene_map,
+    #                              search_window,
+    #                              cols,
+    #                              rows,
+    #                              num_nodes,
+    #                              font_size)
 
-    rrt_planner.run()
+    # rrt_planner.run()
 
 
 if __name__ == '__main__':
