bc6018ef73d0e04362aee42187b4f18306eee986
Marcos Pereira <marcos.si.pereira@gmail.com>
2023-08-10 23:52:40 +0100

    Add LICENSE and headers about license.

--- python-scripts/main.py
+++ python-scripts/main.py
@@ -1,3 +1,13 @@
+#    This code is distributed WITHOUT ANY WARRANTY, without the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU Lesser General Public License for more details.
+  
+#   The license is distributed along with this repository or you can check
+#   <http://www.gnu.org/licenses/> for more details.
+
+# Contributors: 
+# marcos-pereira (https://github.com/marcos-pereira)
+
 #!/usr/bin/env python
 from rtree import index
 # from RRTPlannerMapPyglet import RRTPlanner
